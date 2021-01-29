e400acdaf511d149a598ae2625d1efa702015bea
jhapper <happer@hotmail.com>
2021-01-29 19:02:53 +0800

    login button and backgroun and push to dash

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ThemeProvider as MuiThemeProvider } from '@material-ui/styles';
 import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 import Login from './pages/login';
@@ -28,16 +28,6 @@ function App() {
 						<Route exact path="/signup" component={Signup} />
 					</Switch>
 				</div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-          </ul>
-        </nav>
 			</Router>
 		</MuiThemeProvider>
 	);
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,57 @@
+import 'firebase/auth';
+import firebase from "../firebase"
+import Button from '@material-ui/core/Button';
+import { useHistory } from "react-router-dom";
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function Login() {
-    return <h2>Login</h2>;
-}
+
+	let history = useHistory()
+
+    const signInWithGoogle = () => {
+      const provider = new firebase.auth.GoogleAuthProvider();
+	  auth.signInWithPopup(provider)
+	  .then((user) => {
+		//after we have the credential - lets check if the user exists in firestore
+		var docRef = firestore.collection('users').doc(auth.currentUser.uid);
+		docRef.get().then(doc => {
+		  if (doc.exists) {
+			//user exists then just update the login time
+			return user
+		  } else {
+			//user doesn't exist - create a new user in firestore
+			addNewUserToFirestore(user);        
+		  }
+		})
+	  })
+	  .then(() => {
+		history.push('/')});;
+    }
+  
+    return (
+		<div style={{minHeight: '100vh', backgroundImage: `url('/BaliSpace.jpeg')`, backgroundRepeat: 'no-repeat', backgroundSize: 'cover',}}>
+			<div style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh'}}>
+				<Button variant="contained" color="primary" onClick={signInWithGoogle}>Sign in with Google</Button>
+			</div>
+		</div>
+
+    )
+  
+  }
+
+  function addNewUserToFirestore(user) {
+	const collection = firestore.collection('users');
+    const {profile} = user.additionalUserInfo;
+    const details = {
+      firstName: profile.given_name,
+      lastName: profile.family_name,
+      fullName: profile.name,
+      email: profile.email,
+    };
+    collection.doc(auth.currentUser.uid).set(details);
+    return {user, details};
+  }
 
 export default Login;
